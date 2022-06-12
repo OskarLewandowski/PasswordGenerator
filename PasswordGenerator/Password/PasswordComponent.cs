6f0d417ce346ea4118194112ae1948c5457d42da
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-06-12 16:18:50 +0200

    added class with password components, password model

---
+++ PasswordGenerator/Password/PasswordComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Password
+{
+    public class PasswordComponent
+    {
+        public int HowManyCharactersInTotal { get; set; }
+        public int HowManyDigits { get; set; }
+        public int HowManyLargeLetter { get; set; }
+        public int HowManySmallLetter { get; set; }
+        public int HowManySpecialCharacter { get; set; }
+
+        public PasswordComponent(int howManyCharactersInTotal, int howManyDigits, int howManyLargeLetter,
+            int howManySmallLetter, int howManySpecialCharacter)
+        {
+            HowManyCharactersInTotal = howManyCharactersInTotal;
+            HowManyDigits = howManyDigits;
+            HowManyLargeLetter = howManyLargeLetter;
+            HowManySmallLetter = howManySmallLetter;
+            HowManySpecialCharacter = howManySpecialCharacter;
+        }
+    }
+}
--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -1,3 +1,11 @@
-﻿
-Console.WriteLine("Password Generator!");
+﻿using PasswordGenerator.Password;
+
+PasswordComponent passwordComponents = new PasswordComponent(8, 1, 1, 1, 1);
+Console.WriteLine(
+    $"CharactersInTotal: {passwordComponents.HowManyCharactersInTotal}\n" +
+    $"Digits: {passwordComponents.HowManyDigits}\n" +
+    $"LargeLetter: {passwordComponents.HowManyLargeLetter}\n" +
+    $"SmallLetter: {passwordComponents.HowManySmallLetter}\n" +
+    $"SpecialCharacter: {passwordComponents.HowManySpecialCharacter}\n");
+
 Console.ReadLine();
