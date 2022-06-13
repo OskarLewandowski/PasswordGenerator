ba3f04216e75f8f042654e395de7d8178572b94d
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-06-13 22:45:01 +0200

    added method to own password generation

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -3,6 +3,8 @@ using PasswordGenerator.Extensions;
 using PasswordGenerator.Password;
 
 PasswordComponent passwordDefaultComponent = new PasswordComponent(12, 2, 2, 2, 2);
+PasswordComponent passwordExtendedComponent = new PasswordComponent(0, 0, 0, 0, 0);
+
 string userInput = "";
 Terminal.Display();
 
@@ -28,7 +30,7 @@ while (true)
                 passwordDefaultComponent.Display();
                 break;
             case 3:
-                Terminal.WriteLineColor("Generate password with extended options", ColorName.Blue);
+                MakeExtendedPassword(passwordExtendedComponent);
                 break;
             case 4:
                 Terminal.WriteLineColor("Edit list - not applied", ColorName.Blue);
@@ -62,6 +64,114 @@ while (true)
         else
         {
             Terminal.WriteLineColor("Command not found", ColorName.DarkRed);
+        }
+    }
+}
+
+
+
+static int MakeExtendedPassword(PasswordComponent model)
+{
+    Terminal.WriteLineColor("How much characters in password:", ColorName.Gray);
+    var input = Console.ReadLine();
+    if (int.TryParse(input, out int characters))
+    {
+        model.HowManyCharactersInTotal = characters;
+    }
+    else
+    {
+        Terminal.WriteLineColor("Bad value", ColorName.DarkRed);
+    }
+
+    Terminal.WriteLineColor("How many minimum numbers:", ColorName.Gray);
+    input = Console.ReadLine();
+    if (int.TryParse(input, out int digits))
+    {
+        model.HowManyDigits = digits;
+    }
+    else
+    {
+        Terminal.WriteLineColor("Bad value", ColorName.DarkRed);
+    }
+
+    Terminal.WriteLineColor("How many minimum large letters:", ColorName.Gray);
+    input = Console.ReadLine();
+    if (int.TryParse(input, out int largeLetters))
+    {
+        model.HowManyLargeLetter = largeLetters;
+    }
+    else
+    {
+        Terminal.WriteLineColor("Bad value", ColorName.DarkRed);
+    }
+
+    Terminal.WriteLineColor("How many minimum small letters:", ColorName.Gray);
+    input = Console.ReadLine();
+    if (int.TryParse(input, out int smallLetters))
+    {
+        model.HowManySmallLetter = smallLetters;
+    }
+    else
+    {
+        Terminal.WriteLineColor("Bad value", ColorName.DarkRed);
+    }
+
+    Terminal.WriteLineColor("How many minimum special characters:", ColorName.Gray);
+    input = Console.ReadLine();
+    if (int.TryParse(input, out int specialCharacters))
+    {
+        model.HowManySpecialCharacter = specialCharacters;
+    }
+    else
+    {
+        Terminal.WriteLineColor("Bad value", ColorName.DarkRed);
+    }
+
+    Password password = new Password();
+    var myCharactersList = password.PrepareListOfCharacters(model);
+
+    Terminal.WriteLineColor("How many times to shuffle the list:", ColorName.Gray);
+    input = Console.ReadLine();
+    if (int.TryParse(input, out int shuffle))
+    {
+        myCharactersList.Shuffle(shuffle);
+    }
+    else
+    {
+        Terminal.WriteLineColor("Bad value", ColorName.DarkRed);
+    }
+
+    password.AddPasswordRequirements(model);
+
+    Terminal.WriteLineColor("-\t-\t-\t-\t-\t-\t-\t", ColorName.DarkMagenta);
+    Terminal.WriteLineColor(password.GetPassword(myCharactersList), ColorName.Green);
+    Terminal.WriteLineColor("-\t-\t-\t-\t-\t-\t-\t", ColorName.DarkMagenta);
+
+    Terminal.WriteLineColor("[ 1 ] Generate a new password ", ColorName.Yellow);
+    Terminal.WriteLineColor("[ 2 ] Exit", ColorName.Yellow);
+    Terminal.WriteLineColor("What next:", ColorName.Yellow);
+
+    while (true)
+    {
+        input = Console.ReadLine();
+        if (int.TryParse(input, out int choice))
+        {
+            switch (choice)
+            {
+                case 1:
+                    Terminal.WriteLineColor("-\t-\t-\t-\t-\t-\t-\t", ColorName.DarkMagenta);
+                    Terminal.WriteLineColor(password.GetPassword(myCharactersList), ColorName.Green);
+                    Terminal.WriteLineColor("-\t-\t-\t-\t-\t-\t-\t", ColorName.DarkMagenta);
+                    break;
+                case 2:
+                    return 0;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            Terminal.WriteLineColor("Bad value", ColorName.DarkRed);
         }
     }
 }
